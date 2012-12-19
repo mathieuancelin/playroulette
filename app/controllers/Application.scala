3cc3ba61f79d2c5d093ea00abfeb25cfab49ef92
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2012-12-19 11:01:54 +0100

    Add user broadcast

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,65 +13,86 @@ import scala.concurrent.stm._
 import akka.util.duration._
 import play.api.cache._
 import play.api.libs.json._
-import java.util.concurrent.ConcurrentLinkedQueue
-import java.util.concurrent.ConcurrentHashMap
+import scala.collection.mutable._
 import java.util.concurrent.atomic.AtomicReference
-import java.util._
-import collection.JavaConversions._
+import java.util.concurrent.atomic.AtomicLong
+import java.util.UUID
 
 object Application extends Controller {
 
     val eventSource = Enumeratee.map[String] { msg => "data: " + msg + "\n\n" }
 
+    val number = new AtomicLong(0)
+
     def index() = Action {
         val uid = UUID.randomUUID().toString()
-        val user = User.join(uid, "Anonymous", "")
-        user.waiting()
-        val state = restartUser(user.id)
+        val user = User.join( uid, "Anonymous-" + number.incrementAndGet(), "" )
+        user.waiting( )
+        val state = restartUser( user.id )
         Ok( views.html.index( uid, state ) )
     }
 
     def feed(id: String) = Action { implicit request =>
-        Option( User.users.get( id ) ).map { user =>
+        User.users.get( id ).map { user =>
             Ok.feed( user.feedEnumerator.through( eventSource ) ).as( "text/event-stream" )
         }.getOrElse {
-          NotFound("User not found with id " + id)
+          NotFound( "User not found with id " + id )
         }
     }
 
-    def websocket(id: String) = WebSocket.async[Array[Byte]] { request =>
-        Option( User.users.get( id ) ).map { user =>
+    def websocket( id: String ) = WebSocket.async[Array[Byte]] { request =>
+        User.users.get( id ).map { user =>
           Promise.pure( ( user.inputCameraIteratee, user.outputBroadcastEnumerator.getPatchCord() ) )
         }.getOrElse {
           Promise.pure( ( Iteratee.ignore, Enumerator.eof ) )
         }
     }
 
-    def next(id: String) = Action {
+    def next( id: String ) = Action {
         User.findChat( id ).map { chat =>
             chat.stop()
             restartUser( chat.user1.id )
             restartUser( chat.user2.id )
-            Ok("")
-        }.getOrElse( NotFound("Chat not found for user with id " + id) )
+            Ok( "" )
+        }.getOrElse( NotFound( "Chat not found for user with id " + id ) )
     }
 
     def restartUser( id: String ) = {
-        Option( User.users.get( id ) ).map { user =>
+        User.users.get( id ).map { user =>
             user.waiting()
-            if (User.waitingUsers.isEmpty()) {
-              User.waitingUsers.offer(user)
-              user.informWaiting()
-              "waiting"
+            if ( User.waitingUsers.isEmpty ) {
+                User.waitingUsers += user
+                user.informWaiting()
+                "waiting"
             } else {
-              val otherChatter = User.waitingUsers.poll()
-              val chat = Chat.register( user, otherChatter )
-              chat.start()
-              user.informNewChat(chat.id)
-              otherChatter.informNewChat(chat.id)
-              chat.id
+                val otherChatter = User.waitingUsers.dequeue()
+                val chat = Chat.register( user, otherChatter )
+                chat.start()
+                user.informNewChat( chat.id )
+                otherChatter.informNewChat( chat.id )
+                chat.id 
             }
         }.get
+    }
+
+    def userList() = Action {
+        Ok( views.html.users( User.users.map { user => user._2 }.toList ) )
+    }
+
+    def user( id: String ) = Action {
+        User.users.get( id ).map { user =>
+            Ok( views.html.user( user ) )
+        }.getOrElse {
+          NotFound( "User not found with id " + id )
+        }
+    }
+
+    def userCam( id: String ) = WebSocket.async[Array[Byte]] { request =>
+        User.users.get( id ).map { user =>
+          Promise.pure( ( Iteratee.ignore[Array[Byte]], user.outputUserBroadcastEnumerator.getPatchCord() ) )
+        }.getOrElse {
+          Promise.pure( ( Iteratee.ignore, Enumerator.eof ) )
+        }
     }
 }
 
@@ -84,20 +105,24 @@ case class User(id: String, name: String = "Anonymous", description: String = ""
             optionnalConsumer.get().foreach { consumer =>
                 consumer.pushFrame( message )
             }
+            outputUserEnumerator.push( message )
         }
     }).mapDone({ in =>
         User.removeUser( id )
+        outputEnumerator.close()
+        outputUserEnumerator.close()
+        outputBroadcastEnumerator.close()
         optionnalConsumer.get().foreach { consumer =>
             Application.restartUser( consumer.id )
         }
-        outputEnumerator.close()
-        outputBroadcastEnumerator.close()
     })
 
     val feedEnumerator = Enumerator.imperative[String]() 
 
-    private val outputEnumerator = Enumerator.imperative[Array[Byte]]() 
+    private val outputUserEnumerator = Enumerator.imperative[Array[Byte]]() 
+    val outputUserBroadcastEnumerator = Concurrent.hub[Array[Byte]]( outputUserEnumerator )
 
+    private val outputEnumerator = Enumerator.imperative[Array[Byte]]() 
     val outputBroadcastEnumerator = Concurrent.hub[Array[Byte]]( outputEnumerator )
 
     def pushFrame(frame: Array[Byte]) = outputEnumerator.push( frame )
@@ -113,52 +138,44 @@ case class User(id: String, name: String = "Anonymous", description: String = ""
 
 object User {
 
-    val users = new ConcurrentHashMap[String, User]()
+    val users = HashMap.empty[String, User]
 
-    val waitingUsers = new ConcurrentLinkedQueue[User]()
+    val waitingUsers = new SynchronizedQueue[User]()
 
-    def join(uid: String, name: String, desc: String) = {
-        val user = User(uid, name, desc)
-        if ( !users.containsKey( uid ) ) {
-            users.putIfAbsent( uid, user )
-        }
+    def join( uid: String, name: String, desc: String ) = {
+        val user = User( uid, name, desc )
+        users.put( uid, user )
         user
     }
 
-    def removeUser(id: String) = {
-        if ( users.containsKey( id ) ) {
-            users.remove( id )
-        }
-    }
+    def removeUser(id: String) = users.remove( id )
 
-    def findChat(id: String): Option[Chat] = {
-        for (chat <- Chat.chats.values()) {
-            if(chat.user1.id.equals(id)) return Some(chat)
-            if(chat.user2.id.equals(id)) return Some(chat)
+    def findChat( id: String ): Option[Chat] = {
+        for ( chat <- Chat.chats.values ) {
+            if( chat.user1.id.equals( id ) ) return Some( chat )
+            if( chat.user2.id.equals( id ) ) return Some( chat )
         }
         None
     }
 }
 
-case class Chat(id: String, user1: User, user2: User) {
+case class Chat( id: String, user1: User, user2: User ) {
     def start() {
         user1.connectToNewChatter( user2 )
         user2.connectToNewChatter( user1 )
     }
-    def stop() = Chat.remove(id)
+    def stop() = Chat.remove( id) 
 }
 
 object Chat {  
 
-    val chats = new ConcurrentHashMap[String, Chat]()
+    val chats = HashMap.empty[String, Chat]
 
-    def register(user1: User, user2: User) = {
+    def register( user1: User, user2: User ) = {
         val uid = UUID.randomUUID().toString()
-        val chat = Chat(uid, user1, user2)
-        if (!chats.containsKey(uid)) {
-            chats.putIfAbsent(uid, chat)
-        }
+        val chat = Chat( uid, user1, user2 )
+        chats.put( uid, chat )
         chat
     }
-    def remove(id: String) = if (chats.containsKey(id)) chats.remove(id)
+    def remove( id: String ) = chats.remove( id ) 
 }
