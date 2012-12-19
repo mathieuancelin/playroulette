de3e21ac77e770f90ef7b0c1a9a082b573e38b4f
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2012-12-19 14:53:26 +0100

    Change name

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "websocam"
+    val appName         = "playroulette"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
